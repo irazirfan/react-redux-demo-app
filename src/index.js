c0e42a5ed0725c0f79d2575ad23f4e885d64b18a
Iraz Irfan <iraz@mislbd.com>
2020-04-20 11:16:34 +0600

    Init App Structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import "./index.css";
 
-function Hello() {
-  return <p>Hello World!</p>;
-}
-
-render(<Hello />, document.getElementById("app"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
