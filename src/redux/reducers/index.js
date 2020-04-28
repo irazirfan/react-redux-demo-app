0562c4e7dac109060d033b86619705bf91958993
Iraz Irfan <irazirfan@gmail.com>
2020-04-28 16:08:35 +0600

    Creating CourseList

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import courses from "./courseReducer";
+import authors from "./authorReducers";
 
 const rootReducer = combineReducers({
   courses,
+  authors,
 });
 
 export default rootReducer;
